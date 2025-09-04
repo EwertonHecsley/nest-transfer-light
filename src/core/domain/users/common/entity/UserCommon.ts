13285e84e19b2217d3e132e3b86b47fcb8d35dc5
Ewerton Hecsley <ewerton.martinscomercial@gmail.com>
2025-09-03 22:08:58 -0300

    test: UserCommon

--- src/core/domain/users/common/entity/UserCommon.ts
+++ src/core/domain/users/common/entity/UserCommon.ts
@@ -1,56 +1,58 @@
-import { Entity } from "@/core/generics/Entity";
-import { CPF } from "../objectValues/CPF";
-import { Email } from "../objectValues/Email";
-import { Identity } from "@/core/generics/Identity";
+import { Entity } from '@/core/generics/Entity';
+import { CPF } from '../objectValues/CPF';
+import { Email } from '../objectValues/Email';
+import { Identity } from '@/core/generics/Identity';
 
 type UserCommonProps = {
-    fullName:string;
-    cpf:CPF;
-    email:Email;
-    password:string;
-    common:boolean;
-    createdAt:Date;
-}
+  fullName: string;
+  cpf: CPF;
+  email: Email;
+  password: string;
+  common: boolean;
+  createdAt: Date;
+};
 
+export class UserCommon extends Entity<UserCommonProps> {
+  private constructor(attributes: UserCommonProps, id?: Identity) {
+    super({ ...attributes, createdAt: attributes.createdAt ?? new Date() }, id);
+  }
 
-export class UserCommon extends Entity<UserCommonProps>{
-    private constructor(attributes:UserCommonProps,id?:Identity){
-        super({...attributes,createdAt:attributes.createdAt ?? new Date()},id);
-    }
+  public static create(attributes: UserCommonProps, id?: Identity): UserCommon {
+    return new UserCommon(
+      { ...attributes, createdAt: attributes.createdAt },
+      id,
+    );
+  }
 
-    public static create(attributes:UserCommonProps,id?:Identity):UserCommon{
-        return new UserCommon({...attributes,createdAt:attributes.createdAt},id);
-    }
+  get fullName(): string {
+    return this.attributes.fullName;
+  }
 
-    get fullName():string{
-        return this.attributes.fullName;
-    }
+  get cpf(): CPF {
+    return this.attributes.cpf;
+  }
 
-    get cpf():CPF{
-        return this.attributes.cpf;
-    }
+  get email(): Email {
+    return this.attributes.email;
+  }
 
-    get email():Email{
-        return this.attributes.email;
-    }
+  get password(): string {
+    return this.attributes.password;
+  }
 
-    get password():string{
-        return this.attributes.password;
-    }
+  get userCommon(): boolean {
+    return this.attributes.common;
+  }
 
-    get userCommon():boolean{
-        return this.attributes.common;
-    }
+  get createdAt(): Date {
+    return this.attributes.createdAt;
+  }
 
-    get createdAt():Date{
-        return this.attributes.createdAt;
-    }
+  public updateFullName(fullName: string): void {
+    this.attributes.fullName = fullName;
+  }
 
-    public updateFullName(fullName:string):void{
-        this.attributes.fullName = fullName;
-    }
-
-    public updateEmail(email:Email):void{
-        this.attributes.email = email;
-    }
+  public updateEmail(email: Email): void {
+    this.attributes.email = email;
+  }
 }
