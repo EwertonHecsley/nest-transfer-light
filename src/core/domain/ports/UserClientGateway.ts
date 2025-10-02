252d76924930e25873fb0aa89d9c3685c499f047
Ewerton Hecsley <ewerton.martinscomercial@gmail.com>
2025-10-01 21:55:36 -0300

    feat: use case to list all UserClient

--- src/core/domain/ports/UserClientGateway.ts
+++ src/core/domain/ports/UserClientGateway.ts
@@ -1,8 +1,20 @@
 import { UserClient } from '../userClient/entity/UserClient';
 
+export type FindAllParams = {
+  page: number;
+  limit: number;
+};
+
+export type PaginatedResponse<T> = {
+  data: T[];
+  total: number;
+  page: number;
+  limit: number;
+};
 export abstract class UserClientGateway {
   abstract create(entity: UserClient): Promise<UserClient>;
   abstract findByCpf(cpf: string): Promise<UserClient | null>;
   abstract findByEmail(email: string): Promise<UserClient | null>;
   abstract findById(id: string): Promise<UserClient | null>;
+  abstract listAll(params:FindAllParams):Promise<PaginatedResponse<UserClient>>;
 }
